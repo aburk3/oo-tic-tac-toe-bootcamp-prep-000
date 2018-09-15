26cbc95106594ffd4b5a409ed579bfa9c47651db
Austin  Burke <aburk3@gmail.com>
2018-09-15 19:49:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,31 +1,28 @@
 class TicTacToe
   def initialize
-    @board = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-      ]
-    end
-    
-  def display_board
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts "-----------"
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
-  
+
+  WIN_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
   def input_to_index(user_input)
     user_input.to_i - 1
   end
-  
-  def player_move(index, player)
-    @board[index] = player
+
+  def move(index, token)
+    @board[index] = token
   end
 
   def position_taken?(index)
@@ -33,7 +30,15 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    index.between?(0,8) && !position_taken?(index)
+    !position_taken?(index) && index.between?(0,8)
+  end
+
+  def turn_count
+    @board.count{|square| square != " " }
+  end
+
+  def current_player
+    turn_count.even? ? "X" : "O"
   end
 
   def turn
@@ -49,15 +54,7 @@ class TicTacToe
     display_board
   end
 
-  def turn_count
-    @board.count{|square| square != " " }
-  end
-
-   def current_player
-    turn_count.even? ? "X" : "O"
-  end
-
-   def won?
+  def won?
     WIN_COMBINATIONS.any? do |combo|
       if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
         return combo
