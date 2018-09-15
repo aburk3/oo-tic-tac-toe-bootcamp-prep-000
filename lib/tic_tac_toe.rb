ffb1d482a01cf65048603856c1081dc14c181146
Austin  Burke <aburk3@gmail.com>
2018-09-15 20:09:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,8 +43,12 @@ end
 counter
 end
 
-  def current_player
-    turn_count.even? ? "X" : "O"
+  def current_player(board)
+    if turn_count(board) % 2 == 0
+      "X"
+    else
+      "O"
+    end
   end
 
   def turn
