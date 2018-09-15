e84af14f645eeb551e41995c127f0ce836c007cf
Austin  Burke <aburk3@gmail.com>
2018-09-15 19:40:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,33 +23,33 @@ class TicTacToe
   user_input.to_i - 1
   end
   
-  def player_move(board, index, player)
-  board[index] = player
-end
+  def player_move(index, player)
+  @board[index] = player
+  end
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
+  def position_taken?(index)
+    @board[index] != " "
+  end
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    player_move(board, index, current_player(board))
-    display_board(board)
-  else
-    turn(board)
+  def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      player = current_player
+      move(index, player)
+    else
+      turn
   end
 end
 
-def turn_count(board)
-  counter = 0
-  board.each do |turn|
+  def turn_count
+    counter = 0
+    @board.each do |turn|
   if turn == "X" || turn == "O"
     counter += 1
   end
@@ -57,49 +57,46 @@ end
 counter
 end
 
-def current_player(board)
-  if turn_count(board) % 2 == 0
-   "X"
-  else
-    "O"
+  def current_player
+    if turn_count.even? ? "X" : "O"
   end
-end
 
-def won?(board)
-  WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[1])
+  def won?(board)
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]] &&
+      position_taken?(combo[1])
+      return combo
+    end
   end
-end
   
-def full?(board)
-  board.all? do |location|
-    location == "X" || location == "O"
+  def full?
+    @board.all? do |location|
+      location == "X" || location == "O"
   end
-end
 
-def draw?(board)
-  !won?(board) && full?(board)
-end
 
-def over?(board)
-  won?(board) || draw?(board) || full?(board)
-end
+  def draw?
+    !won? && full?
+  end
+
+  def over?
+    won? || draw? || full?
+  end
  
-def winner(board)
-  if winnerChickenDinner = won?(board)
-    board[winnerChickenDinner.first]
+  def winner
+    if winnerChickenDinner = won?
+      @board[winnerChickenDinner.first]
+    end
   end
-end
 
-def play(board)
-  while !over?(board)
-    turn(board)
+  def play
+    while !over?
+      turn
   end
-  if won?(board)
+  if won?
     puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
+  elsif draw?
     puts "Cat's Game!"
   end
 end
